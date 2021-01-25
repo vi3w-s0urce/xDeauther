b895fce7356f321ab21d6d96d690f9be41639470
xG4L1L30x <macdizofti032@gmail.com>
2021-01-25 20:51:41 +0700

    Update: select target

--- xDeauther.sh
+++ xDeauther.sh
@@ -1,39 +1,38 @@
 #!/bin/sh
 
 #warna
-H='\e[30m'
-R='\e[31m'
-G='\e[32m'
-B='\e[34m'
-P='\e[35m'
-C='\e[36m'
-Y='\e[33m'
-W='\e[97m'
-bgH='\e[40m'
-bgR='\e[41m'
-bgG='\e[42m'
-bgB='\e[44m'
-bgP='\e[45m'
-bgC='\e[46m'
-bgY='\e[43m'
-bgW='\e[107m'
-BOLD='\e[1m'
-RST='\e[0m'
+H='\033[30m'
+R='\033[31m'
+G='\033[32m'
+B='\033[34m'
+P='\033[35m'
+C='\033[36m'
+Y='\033[33m'
+W='\033[97m'
+bgH='\033[40m'
+bgR='\033[41m'
+bgG='\033[42m'
+bgB='\033[44m'
+bgP='\033[45m'
+bgC='\033[46m'
+bgY='\033[43m'
+bgW='\033[107m'
+BOLD='\033[1m'
+RST='\033[0m'
 
 trap quit EXIT
 mkdir log
 
 function interface() {
-  echo -e "[${G}${BOLD}*${RST}] Please select your interface:\n"
-  ip link | grep -E "^[0-9]+" | awk -F':' '{ print $1 $2 }' 1> log/iface.txt
-  sed 's/\(\b[0-9]\)/\1\)/g' log/iface.txt > log/show.txt
-  cat log/show.txt
+  echo -e "${B}${BOLD}Please select interface:${RST}\n"
+  ip link | grep -E "^[0-9]+" | awk -F':' '{ print $2 }' 1> log/iface.txt
+  cat log/iface.txt | awk '{ print $1 }' | awk '{print "[" "\033[32m\033[1m"NR "\033[0m] " $s}'
   echo ""
   echo -ne "${bgR}${H}xDeauther${RST}:${C}${BOLD}Interface${RST} => "; read slct
-  slctinterface=$(grep "$slct" log/iface.txt | awk -F'^[0-9]+' '{ print $2 }' | awk '{ print $1 }')
+  slctinterface=$(sed "$slct!d" log/iface.txt | awk '{ print $1 }')
   iface=$slctinterface
-  if [[ $iface == "" ]]; then
-    echo -e "[${R}${BOLD}!${RST}] Invalid Option!"
+  if [[ $iface == "" ]] && [[ $slct == 0 ]] || [[ $slct =~ [a-zA-Z]+ ]]; then
+    echo -e "[${R}${BOLD}!${RST}] Invalid Option! Please select number."
     interface
   else
     check_mode
@@ -44,6 +43,7 @@ function check_mode() {
   mode=$(iw $iface info | grep "type" )
   if [[ $mode == *'monitor'* ]]; then
     echo -e "[${G}${BOLD}*${RST}] Interface already in monitor mode"
+    target
   else
     echo -e "[${R}${BOLD}!${RST}] Interface is'nt on monitor mode!"
     sleep 1
@@ -56,17 +56,32 @@ function check_mode() {
 
 function change_mode() {
   airmon-ng start $iface > /dev/null 2>&1
+  sleep 5
   chck=$(iw $iface info | grep "type" )
   if [[ $chck == *'monitor'* ]]; then
-    echo -e "[*] Interface has change to monitor mode"
+    echo -e "[*] Success change to monitor mode"
+    target
   elif [[ $chck == *'managed'* ]]; then
     echo -e "[${R}${BOLD}!${RST}] Interface doesnt supported!"
-    airmon-ng stop $iface > /dev/null 2>&1
   fi
+  chckmon=$(ip link | grep -E "^[0-9]+" | awk -F':' '{ print $1 $2 }' | grep "mon")
+  if [[ $chckmon == *'mon'* ]]; then
+    iface="${iface}mon"
+  fi
+}
+
+function target() {
+  xterm -e /bin/bash -l -c "airodump-ng -w log/target --output-format csv ${iface}"
+  cat log/target-01.csv | sed '1d' | cut -d, -f 14,4,1,6 | awk -F',' '{print $4"," $1"," $2"," $3}' > log/showtarget.csv
+  column -s, -t < log/showtarget.csv > log/target.txt
+  cat log/target.txt | awk '{ print "["NR-1"]" $s }' | sed '1s/0/#/'> log/showtarget.txt
+  cat log/showtarget.txt
+  read
 }
 
 function quit() {
   echo -e "\n\nBye"
+  airmon-ng stop $iface > /dev/null 2>&1
   rm -rf log
 }
 
