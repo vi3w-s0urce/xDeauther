68055878ed69714fde8020d5acd08bc9bd1c4f4f
xG4L1L30x <macdizofti032@gmail.com>
2021-01-27 16:06:18 +0700

    Update banner

--- xDeauther.sh
+++ xDeauther.sh
@@ -44,13 +44,14 @@ function interface() {
   ip link | grep -E "^[0-9]+" | awk -F':' '{ print $2 }' 1> log/iface.txt
   cat log/iface.txt | awk '{ print $1 }' | awk '{print "[" "\033[32m\033[1m"NR "\033[0m] " $s}'
   echo ""
-  echo -ne "${bgR}${H}xDeauther${RST}:${C}${BOLD}Interface${RST} => "; read slct
-  slctinterface=$(sed "$slct!d" log/iface.txt | awk '{ print $1 }')
-  iface=$slctinterface
-  if [[ $iface == "" ]] && [[ $slct == 0 ]] || [[ $slct =~ [a-zA-Z]+ ]]; then
-    echo -e "[${R}${BOLD}!${RST}] Invalid Option! Please select number."
+  echo -ne "${bgR}${H}xDeauther${RST}:${C}${BOLD}Interface${RST} => "; read slct_interface
+  echo ""
+  iface=$(sed "$slct_interface!d" log/iface.txt | awk '{ print $1 }')
+  if [[ $iface == "" ]] && [[ $slct_interface == 0 ]] || [[ $slct_interface =~ [a-zA-Z]+ ]]; then
+    echo -e "[${R}${BOLD}!${RST}] Invalid Option! Please select number\n"
     interface
   else
+    loading
     check_mode
   fi
 }
@@ -60,13 +61,15 @@ function check_mode() {
   if [[ $mode == *'monitor'* ]]; then
     echo -e "\n[${G}${BOLD}*${RST}] Interface already in monitor mode"
     sleep 2
+    echo -e "[${G}${BOLD}+${RST}] Set ${C}${BOLD}$iface${RST} to main interface"
+    sleep 2
     clear
     banner
-    target
+    weapons
   else
     echo -e "\n[${R}${BOLD}!${RST}] Interface isnt on monitor mode!"
     sleep 2
-    loading
+    loading2
     change_mode
   fi
 }
@@ -75,10 +78,13 @@ function change_mode() {
   airmon-ng start $iface > /dev/null 2>&1
   chck=$(iw $iface info | grep "type" )
   if [[ $chck == *'monitor'* ]]; then
-    echo -e "[*] Success change to monitor mode"
+    echo -e "\n\n[${G}${BOLD}+${RST}] Success change to monitor mode"
+    sleep 2
+    echo -e "[${G}${BOLD}+${RST}] Set ${C}${BOLD}$iface${RST} to main interface"
+    sleep 2
     clear
     banner
-    target
+    weapons
   elif [[ $chck == *'managed'* ]]; then
     echo -e "\n\n[${R}${BOLD}!${RST}] Interface doesnt supported!"
     sleep 2
@@ -92,10 +98,32 @@ function change_mode() {
   fi
 }
 
+function weapons() {
+  echo -e "-=[ ${Y}${BOLD}SELECT WEAPONS${RST} ]=-\n"
+  echo -e "[${G}${BOLD}1${RST}] mdk4 [${R}${BOLD}BRUTAL${RST}]"
+  echo -e "[${G}${BOLD}2${RST}] mdk3 (Previous version mdk3)"
+  echo -e "[${G}${BOLD}3${RST}] aireplay-ng [${Y}${BOLD}NORMAL${RST}]"
+  echo -ne "\n${bgR}${H}xDeauther${RST}:${C}${BOLD}Weapons${RST} => "; read slct_weapons
+  if [[ $slct_weapons == 1 ]]; then
+    echo -e "\n[${G}${BOLD}+${RST}] Take ${R}${BOLD}mdk4${RST} to be a weapon"
+  elif [[ $slct_weapons == 2 ]]; then
+    echo -e "\n[${G}${BOLD}+${RST}] Take ${R}${BOLD}mdk3${RST} to be a weapon"
+  elif [[ $slct_weapons == 3 ]]; then
+    echo -e "\n[${G}${BOLD}+${RST}] Take ${R}${BOLD}aireplay-ng${RST} to be a weapon"
+  else
+    echo -e "\n[${R}${BOLD}!${RST}] Invalid Option! Please select number\n"
+    weapons
+  fi
+  sleep 2
+  clear
+  banner
+  target
+}
+
 function target() {
   echo -e "-=[ ${Y}${BOLD}EXPLORING TARGET${RST} ]=-\n"
-  loading2&
-  echo -e "[${G}${BOLD}*${RST}]Wait at least 5 second and then press CTRL+C to stop"
+  loading3&
+  echo -e "[${G}${BOLD}*${RST}] Wait at least 5 second and then press CTRL+C to stop"
   xterm -e /bin/bash -l -c "airodump-ng -w log/target --output-format csv ${iface}"
   clear
   banner
@@ -109,12 +137,12 @@ function target() {
   column -s, -t < log/showtarget.csv > log/target.txt
   awk '{ print "[""\033[32m\033[1m"NR-1"\033[0m]" $s }' log/target.txt | sed '1s/0/#/'> log/showtarget.txt
   cat log/showtarget.txt
-  echo -ne "\n${bgR}${H}xDeauther${RST}:${C}${BOLD}Target${RST} => "; read slcttarget
-  slcttarget=$(($slcttarget + 1))
+  echo -ne "\n${bgR}${H}xDeauther${RST}:${C}${BOLD}Target${RST} => "; read slct_target
+  slct_target=$(($slct_target + 1))
 }
 
-function loading2() {
-  for (( i = 0; i <= 12 ; i++ )); do
+function loading3() {
+  for (( i = 0; i <= 10 ; i++ )); do
     echo -ne "[${Y}―${RST}] Exploring for target...\r"
     sleep 0.1
     echo -ne "[\]\r"
@@ -126,9 +154,22 @@ function loading2() {
   done
 }
 
-function loading() {
-  for (( i = 0; i <= 20 ; i++ )); do
+function loading2() {
+  for (( i = 0; i <= 15 ; i++ )); do
     echo -ne "[${Y}―${RST}] Change interface mode to monitor...\r"
+    sleep 0.1
+    echo -ne "[\]\r"
+    sleep 0.1
+    echo -ne "[${Y}￨${RST}]\r"
+    sleep 0.1
+    echo -ne "[${Y}/${RST}]\r"
+    sleep 0.1
+  done
+}
+
+function loading() {
+  for (( i = 0; i <= 8 ; i++ )); do
+    echo -ne "[${Y}―${RST}] Check interface mode...\r"
     sleep 0.1
     echo -ne "[\]\r"
     sleep 0.1
