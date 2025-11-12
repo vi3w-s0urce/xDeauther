e9b720872d6298b570893833c7d877384de5430e
Mike <macdizofti032@gmail.com>
2025-11-12 13:12:05 +0700

    change my old username again

--- install.sh
+++ install.sh
@@ -32,8 +32,8 @@ ${R}▒██   ██▒▓█████▄ ▓█████ ▄▄▄     
 ░░   ░▒ ░ ░ ▒  ▒  ░ ░  ░ ▒   ▒▒ ░░░▒░ ░ ░     ░     ▒ ░▒░ ░ ░ ░  ░  ░▒ ░ ▒░
 ░    ░   ░ ░  ░    ░    ░   ▒    ░░░ ░ ░   ░       ░  ░░ ░   ░     ░░   ░
 ░    ░     ░       ░  ░     ░  ░   ░               ░  ░  ░   ░  ░   ░
-        ░${RST}                   ${G}${BOLD}~=:by G4L1L30:=~${RST}
-              > ${C}\033[4mhttps://github.com/xG4L1L30x/xDeauther${RST} <
+        ░${RST}                   ${G}${BOLD}~=:by vi3ws0urce:=~${RST}
+              > ${C}\033[4mhttps://github.com/vi3w-s0urce/xDeauther${RST} <
 """
 echo -e "-=[ ${Y}${BOLD}INSTALL DEPENDENCIES${RST} ]=-\n"
 apt install xterm -y
